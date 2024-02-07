c8683a6e3bca7a43dbe887ace69d8a88714b05cf
almasud <dev.almasud@gmail.com>
2024-02-07 18:28:49 +0600

    feat: Solve the issue of 27. Remove Element.

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,11 @@
-import daily_code_challenge.DailyCodeChallenge
+import leet_code.top_inverview_150.TopInterView150
 
 /**
  * The main method where we call the solution function to evaluate the result
  */
 fun main() {
-    print(DailyCodeChallenge.frequencySort("Aabb"))
+    val nums = intArrayOf(3,2,2,3)
+    val value = 3
+    println(TopInterView150.removeElement(nums, value))
+    nums.forEach { print("$it ") }
 }
