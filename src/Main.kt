1f0ebc359baf1ce09e41a4f6e642e74b059b029c
almasud <dev.almasud@gmail.com>
2024-02-07 13:06:19 +0600

    feat: Solve the daily code challenge "451. Sort Characters By Frequency" fo LeetCode.

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,8 @@
-import leet_code.top_inverview_150.TopInterView150
+import daily_code_challenge.DailyCodeChallenge
 
 /**
  * The main method where we call the solution function to evaluate the result
  */
 fun main() {
-    val nums1 = intArrayOf(1, 2, 3, 0, 0, 0)
-//    val nums1 = intArrayOf(0,0,0,0,0)
-    val nums2 = intArrayOf(2, 5, 6)
-//    val nums2 = intArrayOf(1,2,3,4,5)
-    val m = 3
-    val n = 3
-
-    TopInterView150.merge(nums1, m, nums2, n)
+    print(DailyCodeChallenge.frequencySort("Aabb"))
 }
